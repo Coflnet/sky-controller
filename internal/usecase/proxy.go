e10951d1734e4b14bf039f0745fdba5952b23e2e
Florian Muehlhans <muehlhans.f@coflnet.com>
2023-07-22 21:36:37 +0200

    take node count into account when scaling the proxy

--- internal/usecase/proxy.go
+++ internal/usecase/proxy.go
@@ -1,170 +1,192 @@
 package usecase
 
 import (
+	"context"
 	"time"
-  "context"
 
 	"github.com/rs/zerolog/log"
 
+	"github.com/Coflnet/sky-controller/internal/metrics"
+	"github.com/Coflnet/sky-controller/internal/utils"
 	api "github.com/Coflnet/sky-controller/target/proxy"
-  "github.com/Coflnet/sky-controller/internal/utils"
-  "github.com/Coflnet/sky-controller/internal/metrics"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
-  metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type ProxyScaler struct {
-  Interval time.Duration
-  DesiredReplicas int
-  CurrentReplicas int
+	Interval        time.Duration
+	DesiredReplicas int
+	CurrentReplicas int
 }
 
 var (
-  proxyClient *api.Client
-  clientset *kubernetes.Clientset
+	proxyClient *api.Client
+	clientset   *kubernetes.Clientset
 )
 
 func (p *ProxyScaler) Start() {
 
-  p.init()
+	p.init()
 
-  go func () {
-    for {
-      err := p.scaleIfNecessary()    
+	go func() {
+		for {
+			err := p.scaleIfNecessary()
 
-      if err != nil {
-        log.Error().Err(err).Msgf("Error while scaling proxy")
-      }
+			if err != nil {
+				log.Error().Err(err).Msgf("Error while scaling proxy")
+			}
 
-      time.Sleep(p.Interval)
-    }
-  }()
+			time.Sleep(p.Interval)
+		}
+	}()
 }
 
 func (p *ProxyScaler) init() {
 
-  // init api client
-  var err error
-  proxyClient, err = api.NewClient(utils.ProxyBaseURL())
-  if err != nil {
-    log.Panic().Err(err).Msg("Failed to create proxy client")
-  }
+	// init api client
+	var err error
+	proxyClient, err = api.NewClient(utils.ProxyBaseURL())
+	if err != nil {
+		log.Panic().Err(err).Msg("Failed to create proxy client")
+	}
 
-  // creates the in-cluster config
+	// creates the in-cluster config
 	config, err := rest.InClusterConfig()
 	if err != nil {
-    log.Panic().Err(err).Msg("Failed to create in-cluster config")
+		log.Panic().Err(err).Msg("Failed to create in-cluster config")
 	}
 	// creates the clientset
 	clientset, err = kubernetes.NewForConfig(config)
 	if err != nil {
-    log.Panic().Err(err).Msg("Failed to create kubernetes client")
+		log.Panic().Err(err).Msg("Failed to create kubernetes client")
 	}
 }
 
 func (p *ProxyScaler) scaleIfNecessary() error {
 
-  // update desired and current replica count
-  err := p.updateReplicaCounts()
-  if err != nil {
-    return err
-  }
+	// update desired and current replica count
+	err := p.updateReplicaCounts()
+	if err != nil {
+		return err
+	}
 
-  // update if necessary
-  if p.DesiredReplicas != p.CurrentReplicas {
-    log.Info().Msgf("Scaling proxy from %d to %d replicas", p.CurrentReplicas, p.DesiredReplicas)
-    err = p.scale()
-    if err != nil {
-      return err
-    }
-  }
+	// update if necessary
+	if p.DesiredReplicas != p.CurrentReplicas {
+		log.Info().Msgf("Scaling proxy from %d to %d replicas", p.CurrentReplicas, p.DesiredReplicas)
+		err = p.scale()
+		if err != nil {
+			return err
+		}
+	}
 
-  return nil
+	return nil
 }
 
 func (p *ProxyScaler) scale() error {
-  ctx, cancel := context.WithTimeout(context.Background(), 10 * time.Second)
-  defer cancel()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 
-  // get deployment
-  deployment, err := clientset.AppsV1().Deployments("sky").Get(ctx, "sky-proxy", metav1.GetOptions{})
+	// get deployment
+	deployment, err := clientset.AppsV1().Deployments("sky").Get(ctx, "sky-proxy", metav1.GetOptions{})
 
-  if err != nil {
-    return err
-  }
+	if err != nil {
+		return err
+	}
 
-  // scale deployment
-  deployment.Spec.Replicas = int32Ptr(int32(p.DesiredReplicas))
-  _, err = clientset.AppsV1().Deployments("sky").Update(ctx, deployment, metav1.UpdateOptions{})
+	// scale deployment
+	deployment.Spec.Replicas = int32Ptr(int32(p.DesiredReplicas))
+	_, err = clientset.AppsV1().Deployments("sky").Update(ctx, deployment, metav1.UpdateOptions{})
 
-  if err != nil {
-    log.Error().Err(err).Msg("Failed to update proxy deployment")
-    return err
-  }
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to update proxy deployment")
+		return err
+	}
 
-  return nil
+	return nil
 }
 
 func (p *ProxyScaler) updateReplicaCounts() error {
-  ctx, cancel := context.WithTimeout(context.Background(), 10 * time.Second)
-  defer cancel()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 
+	// execute both requests in parallel
+	// and return for an error if one occurs
 
-  // execute both requests in parallel
-  // and return for an error if one occurs
+	errChan := make(chan error, 2)
 
-  errChan := make(chan error, 2)
+	go func(c context.Context) {
+		err := p.updateCurrentReplicaCount(c)
+		errChan <- err
+	}(ctx)
 
-  go func(c context.Context) {
-    err := p.updateCurrentReplicaCount(c)
-    errChan <- err
-  }(ctx)
+	go func(c context.Context) {
+		err := p.updateDesiredReplicaCount(c)
+		errChan <- err
+	}(ctx)
 
-  go func(c context.Context) {
-    err := p.updateDesiredReplicaCount(c)
-    errChan <- err
-  }(ctx)
+	for i := 0; i < 2; i++ {
+		err := <-errChan
+		if err != nil {
+			return err
+		}
+	}
 
-  for i := 0; i < 2; i++ {
-    err := <-errChan
-    if err != nil {
-      return err
-    }
-  }
-
-  return nil
+	return nil
 }
 
 func (p *ProxyScaler) updateDesiredReplicaCount(ctx context.Context) error {
-  count, err := proxyClient.BaseKeysPartyCountGet(ctx, api.BaseKeysPartyCountGetParams{
-    Party: "hypixel",
-  })
+	proxyCount, err := proxyClient.BaseKeysPartyCountGet(ctx, api.BaseKeysPartyCountGetParams{
+		Party: "hypixel",
+	})
 
-  if err != nil {
-    return err
-  }
+	if err != nil {
+		return err
+	}
+	log.Info().Msgf("Found %d keys for party hypixel", proxyCount)
 
-  log.Debug().Msgf("Found %d keys for party hypixel", count)
-  p.DesiredReplicas = int(count)
-  return nil
+	nodeCount, err := p.listNodeCount(ctx)
+	if err != nil {
+		return err
+	}
+
+	count := int(proxyCount)
+	if nodeCount < count {
+		count = nodeCount
+	}
+
+	log.Info().Msgf("Found %d keys for party hypixel", count)
+	p.DesiredReplicas = int(count)
+	return nil
+}
+
+func (p *ProxyScaler) listNodeCount(ctx context.Context) (int, error) {
+	nodes, err := clientset.CoreV1().Nodes().List(ctx, metav1.ListOptions{})
+
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to list nodes")
+		return 0, err
+	}
+
+	log.Info().Msgf("Found %d nodes", len(nodes.Items))
+	return len(nodes.Items), nil
 }
 
 func (p *ProxyScaler) updateCurrentReplicaCount(ctx context.Context) error {
-  // get deployment
-  deployment, err := clientset.AppsV1().Deployments("sky").Get(ctx, "sky-proxy", metav1.GetOptions{})
+	// get deployment
+	deployment, err := clientset.AppsV1().Deployments("sky").Get(ctx, "sky-proxy", metav1.GetOptions{})
 
-  if err != nil {
-    log.Error().Err(err).Msg("Failed to get proxy deployment")
-    return err
-  }
-  
-  log.Debug().Msgf("Found %d replicas for proxy deployment", deployment.Status.Replicas)
-  p.CurrentReplicas = int(deployment.Status.Replicas)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to get proxy deployment")
+		return err
+	}
 
-  metrics.UpdateProxyReplicas(p.CurrentReplicas)
-  return nil
+	log.Debug().Msgf("Found %d replicas for proxy deployment", deployment.Status.Replicas)
+	p.CurrentReplicas = int(deployment.Status.Replicas)
+
+	metrics.UpdateProxyReplicas(p.CurrentReplicas)
+	return nil
 }
 
 func int32Ptr(i int32) *int32 { return &i }
